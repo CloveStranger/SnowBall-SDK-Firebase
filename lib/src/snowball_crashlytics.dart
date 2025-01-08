aa706afe2f427fd00b427451e5061c95401931ed
dcdev_mini <dcdev_flutter@desktop-zhuziang-mac-mini.local>
2025-01-08 11:59:51 +0800

    Add SnowBall Crashlytics

--- lib/src/snowball_crashlytics.dart
+++ lib/src/snowball_crashlytics.dart
@@ -1,3 +1,9 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+import 'package:logger/logger.dart';
+
+import 'snowball_logger.dart';
+
 class SnowballCrashlytics {
   factory SnowballCrashlytics() {
     return SnowballCrashlytics._makeInstance();
@@ -14,4 +20,32 @@ class SnowballCrashlytics {
 
   static SnowballCrashlytics get instance =>
       SnowballCrashlytics._makeInstance();
+
+  Logger get logger => SnowballLogger.logger;
+
+  final String _prefix = '==> Firebase Crashlytics Log ';
+
+  void _logError(String message, [Object? error, StackTrace? stackTrace]) {
+    logger.e('$_prefix$message', error: error, stackTrace: stackTrace);
+  }
+
+  void _recordError(FlutterErrorDetails error) {
+    if (!kDebugMode) {
+      FirebaseCrashlytics.instance.recordFlutterFatalError(error);
+    }
+    _logError('FlutterError $error');
+  }
+
+  bool _recordAsyncError(Object error, StackTrace stack) {
+    if (!kDebugMode) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    }
+    _logError('FlutterAsyncError', error, stack);
+    return true;
+  }
+
+  void init() {
+    FlutterError.onError = _recordError;
+    PlatformDispatcher.instance.onError = _recordAsyncError;
+  }
 }
