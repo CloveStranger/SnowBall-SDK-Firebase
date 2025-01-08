748990f0f41dae8d172294f382f6f07020ca3896
dcdev_mini <dcdev_flutter@desktop-zhuziang-mac-mini.local>
2025-01-08 12:00:50 +0800

    Add Firebase Core

--- lib/src/snowball_firebase_core.dart
+++ lib/src/snowball_firebase_core.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+
 class SnowballFirebaseCore {
   factory SnowballFirebaseCore() {
     return SnowballFirebaseCore._makeInstance();
@@ -14,4 +16,8 @@ class SnowballFirebaseCore {
 
   static SnowballFirebaseCore get instance =>
       SnowballFirebaseCore._makeInstance();
+
+  Future<void> init(FirebaseOptions firebaseOptions) async {
+    await Firebase.initializeApp(options: firebaseOptions);
+  }
 }
